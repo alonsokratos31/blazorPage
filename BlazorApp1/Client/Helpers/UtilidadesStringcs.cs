4d8fac91f4a888257b0fab777938c7d5a9bb6f16
alonso.garcia025 <alonso.garcia025@gmail.com>
2019-11-12 22:53:17 -0300

    primer commit

---
+++ BlazorApp1/Client/Helpers/UtilidadesStringcs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Client.Helpers
+{
+    public class UtilidadesStringcs
+    {
+        public static string Enmayuscular(string valor) => valor.ToUpper();
+    }
+}
---
+++ BlazorApp1/Shared/Entidades/Peliculas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp1.Shared.Entidades
+{
+    public class Peliculas
+    {
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+    }
+}
