4ce12c5fad37214fc4b5de2c6cf85237b89857d9
alonso.garcia025 <alonso.garcia025@gmail.com>
2019-12-11 17:56:22 -0300

    Inicio de Formularios.

---
+++ BlazorApp1/Shared/Entidades/Genero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlazorApp1.Shared.Entidades
+{
+    public class Genero
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        public string Nombre { get; set; }
+    }
+}
