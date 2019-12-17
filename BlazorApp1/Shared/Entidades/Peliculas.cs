8f00b4971f0ce26d226c5c24482a4bb59257103e
alonso.garcia025 <alonso.garcia025@gmail.com>
2019-12-17 18:29:29 -0300

    Se modifica componente de peliculas

--- BlazorApp1/Shared/Entidades/Peliculas.cs
+++ BlazorApp1/Shared/Entidades/Peliculas.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlazorApp1.Shared.Entidades
 {
     public class Peliculas
     {
+        public int id { get; set; }
+        [Required]
         public string Titulo { get; set; }
-        public DateTime Lanzamiento { get; set; }
+        public string Reseumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string Trailer { get; set; }
+        [Required]
+        public DateTime? Lanzamiento { get; set; }
         public string Poster { get; set; }
         public string TituloCortado
         {
